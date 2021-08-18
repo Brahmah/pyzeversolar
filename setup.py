bf075b3fdec79d9f7a3bcf392929b3b2ec21e86b
Bashir <37988348+Brahmah@users.noreply.github.com>
2021-08-18 11:46:03 +1000

    0.0.2

--- setup.py
+++ setup.py
@@ -5,18 +5,19 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pyzeversolar",
-    version="0.0.01",
+    version="0.0.2",
     author="brahmah",
     author_email="brahmah90@gmail.com",
     description="Library to communicate with ZeverSolar inverters",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/brahmah/pyzeversolar",
-    packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
     ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
     python_requires='>=3.6',
 )
